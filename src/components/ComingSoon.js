3574bf236bf6255629c47a72d0c280859f064edb
Mohsen Yousefian <me@manzik.com>
2021-07-25 13:53:52 -0500

    Fixed devChamps background pic positioning

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,13 +1,17 @@
 import logo from '@/resources/images/devclub-logo.png';
 import '@/styles/ComingSoon.css';
 
+import { Link } from "react-router-dom";
+
 function ComingSoon() {
   return (
     <div className="coming-soon-container">
       <header className="coming-soon-content">
         <img src={logo} className="devclub-logo" alt="logo" />
         <p>
-          Coming soon!
+          Under construction...
+          <br />
+          But for now, check out <Link to="/devchamps">.DevChamps</Link>!
         </p>
       </header>
     </div>
