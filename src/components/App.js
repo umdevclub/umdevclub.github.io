6fd7a4228676236907a43b48966b855978462140
Mohsen Yousefian <me@manzik.com>
2021-07-25 11:21:41 -0500

    Updated the landing page to coming soon

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import logo from '@/resources/images/logo.svg';
+import logo from '@/resources/images/devclub-logo.png';
 import '@/styles/App.css';
 
 function App() {
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming soon!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
       </header>
     </div>
   );
