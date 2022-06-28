03dadc22b90fae5932ccc49c69b08e725663e375
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2022-06-28 18:26:17 -0500

    Video background added.
    Placeholder code for text element in Home page before styles added. NEED TO CHANGE AFTRER STYLES ARE ADDED

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,22 @@
 import "@/styles/Home.scss";
+import backgroundVideo from "@/resources/videos/wavy_video_background.mp4";
 
 import React from "react";
 
 function Home() {
   return (
     <div className="home-container">
-      <h1>Welcome to the Home Page!</h1>
-      <p>This is the home page.</p>
+      <h1>Welcome to .devClub!</h1>
+      <video
+        className="home-video"
+        width="100%"
+        height="100%"
+        autoPlay
+        loop
+        muted
+      >
+        <source type="video/mp4" src={backgroundVideo} />
+      </video>
     </div>
   );
 }
