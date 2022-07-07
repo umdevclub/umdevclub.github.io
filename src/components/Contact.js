7fdaf9a7be3e7ef5051a7b21b696ceb430fe676c
Mohsen Yousefian <yousefim@myumanitoba.ca>
2022-07-07 17:04:13 -0500

    Added back the right merge conflict resolving code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -119,18 +119,19 @@ function Form() {
 function Contact() {
   return (
     <div className="contact-container">
-      <h1>Welcome to the Contact Page!</h1>
-      <p>This is the contact page.</p>
-      <video
-        className="home-video"
-        width="100%"
-        height="100%"
-        autoPlay
-        loop
-        muted
-      >
-        <source type="video/mp4" src={backgroundVideo} />
-      </video>
+      <Form />
+      <div>
+        <video
+          className="home-video"
+          width="100%"
+          height="100%"
+          autoPlay
+          loop
+          muted
+        >
+          <source type="video/mp4" src={backgroundVideo} />
+        </video>
+      </div>
     </div>
   );
 }
