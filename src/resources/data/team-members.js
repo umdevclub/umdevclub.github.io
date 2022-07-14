81d91c4f3bb3e9ed2c033b892b107244d6ca673c
Mohsen Yousefian <yousefim@myumanitoba.ca>
2022-07-14 17:23:39 -0500

    Added the what-we-do section

---
+++ src/resources/data/team-members.js
@@ -0,0 +1,61 @@
+import ahmed from "@/resources/images/team-members/ahmed.png";
+import graham from "@/resources/images/team-members/graham.png";
+import max from "@/resources/images/team-members/max.png";
+import minh from "@/resources/images/team-members/minh.png";
+import mohsen from "@/resources/images/team-members/mohsen.png";
+import sarah from "@/resources/images/team-members/sarah.png";
+import tano from "@/resources/images/team-members/tano.png";
+import tonmoy from "@/resources/images/team-members/tonmoy.png";
+
+const teamMembers = [
+  {
+    name: "Mohsen Yousefian",
+    role: "President",
+    image: mohsen,
+    bio: "Very good person, I like this person, they do cool stuff and it's fun to hang out with them, they are also very smart and active and enjoy doing stuff.",
+  },
+  {
+    name: "Khuc Nguyen (Tano)",
+    role: "Vice-President",
+    image: tano,
+    bio: "Very good person, I like this person, they do cool stuff and it's fun to hang out with them, they are also very smart and active and enjoy doing stuff.",
+  },
+  {
+    name: "Ahmed S. Ahmed",
+    role: "Treasurer",
+    image: ahmed,
+    bio: "Very good person, I like this person, they do cool stuff and it's fun to hang out with them, they are also very smart and active and enjoy doing stuff.",
+  },
+  {
+    name: "Sarah Veloso",
+    role: "Internal",
+    image: sarah,
+    bio: "Very good person, I like this person, they do cool stuff and it's fun to hang out with them, they are also very smart and active and enjoy doing stuff.",
+  },
+  {
+    name: "Tonmoy Ghosh",
+    role: "External",
+    image: tonmoy,
+    bio: "Very good person, I like this person, they do cool stuff and it's fun to hang out with them, they are also very smart and active and enjoy doing stuff.",
+  },
+  {
+    name: "Graham Downey",
+    role: "Events",
+    image: graham,
+    bio: "Very good person, I like this person, they do cool stuff and it's fun to hang out with them, they are also very smart and active and enjoy doing stuff.",
+  },
+  {
+    name: "Max Abdulkhalikov",
+    role: "Promotions",
+    image: max,
+    bio: "Very good person, I like this person, they do cool stuff and it's fun to hang out with them, they are also very smart and active and enjoy doing stuff.",
+  },
+  {
+    name: "Hoang Minh Phan",
+    role: "Vice-executive",
+    image: minh,
+    bio: "Very good person, I like this person, they do cool stuff and it's fun to hang out with them, they are also very smart and active and enjoy doing stuff.",
+  },
+];
+
+export default teamMembers;
