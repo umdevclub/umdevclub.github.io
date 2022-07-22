b8f4092e0c712789cd7e51dfa4d1191344dd23e6
Maxim Abdulkhalikov <63835313+maxakadem1@users.noreply.github.com>
2022-07-22 18:31:05 -0500

    Contact page is responsive now

--- src/resources/data/homepage-mission-images.js
+++ src/resources/data/homepage-mission-images.js
@@ -1,25 +1,27 @@
+import saul from "@/resources/images/other/saul.gif";
+
 export const workshopImages = [
   {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    img: saul,
     title: "Breakfast",
     rows: 2,
     cols: 2,
   },
   {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    img: saul,
     title: "Burger",
   },
   {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    img: saul,
     title: "Camera",
   },
   {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    img: saul,
     title: "Hats",
     cols: 2,
   },
   {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    img: saul,
     title: "Honey",
     author: "@arwinneil",
     rows: 2,
@@ -29,26 +31,26 @@ export const workshopImages = [
 
 export const hackathonImages = [
   {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    img: saul,
     title: "Breakfast",
     rows: 2,
     cols: 2,
   },
   {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    img: saul,
     title: "Burger",
   },
   {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    img: saul,
     title: "Camera",
   },
   {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    img: saul,
     title: "Hats",
     cols: 2,
   },
   {
-    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    img: saul,
     title: "Honey",
     author: "@arwinneil",
     rows: 2,
