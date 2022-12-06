589da5389171c91bf2b1508c247c9e4ac591a62d
Mohsen Yousefian <yousefim@myumanitoba.ca>
2022-12-05 20:09:27 -0600

    Updated devChamps champions

--- src/resources/data/champions.js
+++ src/resources/data/champions.js
@@ -1,25 +1,32 @@
-import champion1 from "@/resources/images/champions/champion1.png";
-import champion2 from "@/resources/images/champions/champion2.jpg";
-import champion3 from "@/resources/images/champions/champion3.png";
+import graham from "@/resources/images/champions/graham.png";
+import joshua from "@/resources/images/champions/joshua.jpg";
+import brooklynn from "@/resources/images/champions/brooklynn.png";
+import jacob from "@/resources/images/champions/jacob.jpeg";
 
 const champions = [
   {
     name: "Graham Downey",
     role: "Website Challenge",
-    image: champion1,
+    image: graham,
     bio: "Graham developed Alathra, a worldbuilding Minecraft server with a mix of geopolitics and light roleplay, focused on creating an interesting history from the events and actions of players within the land of Alathra.",
   },
   {
     name: "Joshua Smallwood",
     role: "Discord Bot Challenge",
-    image: champion2,
+    image: joshua,
     bio: "Joshue developed Join Sound Johnson, a Discord bot that allows you to set a YouTube link to a video that plays when you connect to a voice channel.",
   },
   {
-    name: "TBD",
-    role: "Generative Art Challenge",
-    image: champion3,
-    bio: "To be determined.",
+    name: "Brooklynn Wood",
+    role: "Generative Art Challenge (Best Art)",
+    image: brooklynn,
+    bio: "Brooklynn created a generative art piece utilizing golden spiral (based on the Fibonacci sequence) rendered using moving squares in the gold and blue color.",
+  },
+  {
+    name: "Jacob Janzen",
+    role: "Generative Art Challenge (Best Code)",
+    image: jacob,
+    bio: 'Jacob created a generative art piece named "Writhing Mass of Flesh" that uses cellular noise rendered with bubbly look to achieve a "flesh" looking animation in his art.',
   },
 ];
 
