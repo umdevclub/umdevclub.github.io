adaa362585c7c1ceb8289ad5d35851b7793659d3
Atishaya Maharjan <atishaya7777@gmail.com>
2023-02-23 21:12:24 -0600

    hotfix: Fixed the bug where the hyperlink wasn't being rendered correctly in the code of conduct

--- src/resources/data/code-of-conduct.js
+++ src/resources/data/code-of-conduct.js
@@ -1,24 +1,42 @@
 const codeOfConduct = [
   {
-    item: "Harassment of any form will not be tolerated. This includes verbal, physical, or emotional discrimination related to gender, gender identity and expression, age, sexual orientation, disability, physical appearance, race, ethnicity, nationality, or religion.",
+    item: () => (
+      <>
+        Harassment of any form will not be tolerated. This includes verbal,
+        physical, or emotional discrimination related to gender, gender identity
+        and expression, age, sexual orientation, disability, physical
+        appearance, race, ethnicity, nationality, or religion.
+      </>
+    ),
     subItem: [
       "Participants asked to stop any harassing behavior are expected to comply immediately. Failure to comply may result in removal and expulsion from participating in the hackathon.",
     ],
   },
   {
-    item: "Photography is welcome, but should be met with consent from all those photographed. This also applies to any social media postings – must ensure consent from all of those photographed.",
+    item: () => (
+      <>
+        Photography is welcome, but should be met with consent from all those
+        photographed. This also applies to any social media postings – must
+        ensure consent from all of those photographed.
+      </>
+    ),
     subItem: [],
   },
   {
-    item: `Academic dishonesty of any form will not be tolerated. All participants must follow and meet the University of Manitoba’s Academic Integrity Guidelines: ${(
-      <a
-        href="https://umanitoba.ca/student-supports/academic-supports/academic-integrity."
-        target="_blank"
-        rel="noreferrer"
-      >
-        https://umanitoba.ca/student-supports/academic-supports/academic-integrity.
-      </a>
-    )}`,
+    item: () => (
+      <>
+        Academic dishonesty of any form will not be tolerated. All participants
+        must follow and meet the University of Manitoba’s Academic Integrity
+        Guidelines:
+        <a
+          href="https://umanitoba.ca/student-supports/academic-supports/academic-integrity."
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://umanitoba.ca/student-supports/academic-supports/academic-integrity.
+        </a>
+      </>
+    ),
     subItem: [
       "Failure to comply can result in removal and expulsion from participating in the hackathon.",
     ],
