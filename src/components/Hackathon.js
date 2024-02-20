007a0aea1e7ea63c4e40e830f1ffd0053d55bff5
Atishaya Maharjan <atishaya7777@gmail.com>
2024-02-20 16:01:02 -0600

    feat: Added the devhacks schedule

--- src/components/Hackathon.js
+++ src/components/Hackathon.js
@@ -47,18 +47,6 @@ function Hackathon() {
           fun filled development!
         </h3>
         <div className="hackathon-welcome btn-wrapper">
-          {/* <Button
-            onClick={() => history.push("/schedule")}
-            variant="contained"
-            sx={{
-              fontSize: "1.5rem",
-              fontFamily: "inherit",
-              mt: 3,
-              textTransform: "lowercase", 
-            }}
-          >
-            Schedule
-          </Button> */}
           <Button
             onClick={handleButtonClick}
             variant="contained"
@@ -82,6 +70,18 @@ function Hackathon() {
             }}
           >
             Github tutorial
+          </Button>
+          <Button
+            onClick={() => history.push("/schedule")}
+            variant="contained"
+            sx={{
+              fontSize: "1.5rem",
+              fontFamily: "inherit",
+              mt: 3,
+              textTransform: "lowercase",
+            }}
+          >
+            Schedule
           </Button>
         </div>
       </div>
