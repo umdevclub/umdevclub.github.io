3b4b500f575d9bd9fd44f5ac8dc4621c1814170d
JordonH03 <jordonhong03@gmail.com>
2025-04-25 14:36:36 -0500

    chore!: Update react router config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { reactRouter } from "@react-router/dev/vite";
 import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactRouter(), tsconfigPaths()],
   build: {
     outDir: "build",
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   optimizeDeps: {
