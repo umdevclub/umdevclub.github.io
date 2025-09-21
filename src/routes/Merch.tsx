e5ae233924e1a038257b046bd8cacb277ad07973
FawazBinSaleem <fawaz.orton66@gmail.com>
2025-09-20 20:28:25 -0500

    refactor:updated merch link

--- src/routes/Merch.tsx
+++ src/routes/Merch.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const Merch = () => {
   useEffect(() => {
-    window.location.replace("https://forms.gle/RoLQb1MhVuBS72Uq6");
+    window.location.replace("https://forms.gle/XysX2bZv1Hx2ndcb8");
   }, []);
 
   return null;
