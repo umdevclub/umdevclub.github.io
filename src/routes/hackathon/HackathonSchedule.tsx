86a942a9d8087d72248428466f9cedefaad2a104
FawazBinSaleem <fawaz.orton66@gmail.com>
2026-01-28 18:08:46 -0600

    fix:wrong filepath

--- src/routes/hackathon/HackathonSchedule.tsx
+++ src/routes/hackathon/HackathonSchedule.tsx
@@ -2,26 +2,32 @@ import { useLoaderData } from "react-router-dom";
 
 import Devhacks2024Schedule from "@/resources/data/devHacksArchive/devHacks2024/Schedule2024";
 import Devhacks2025Schedule from "@/resources/data/devHacksArchive/devHacks2025/Schedule2025";
-import Devhacks2026Schedule from "@/resources/data/devHacksArchive/devHacks2026/schedule2026";
+import Devhacks2026Schedule from "@/resources/data/devHacksArchive/devHacks2026/Schedule2026";
 
 import { HackathonConfig } from "@/resources/data/types";
 
-const scheduleMap: Record<string, JSX.Element> = {
-  "2024": <Devhacks2024Schedule />,
-  "2025": <Devhacks2025Schedule />,
-  "2026": <Devhacks2026Schedule />,
-};
+const scheduleMap = {
+  "2024": Devhacks2024Schedule,
+  "2025": Devhacks2025Schedule,
+  "2026": Devhacks2026Schedule,
+} as const;
+
+type YearKey = keyof typeof scheduleMap;
 
 function HackathonSchedule() {
   const { data } = useLoaderData() as HackathonConfig;
 
-  return (
-    scheduleMap[data.year] ?? (
+  const ScheduleComponent = scheduleMap[data.year as YearKey];
+
+  if (!ScheduleComponent) {
+    return (
       <p style={{ textAlign: "center" }}>
         Schedule not available for {data.year}
       </p>
-    )
-  );
+    );
+  }
+
+  return <ScheduleComponent />;
 }
 
 export default HackathonSchedule;
